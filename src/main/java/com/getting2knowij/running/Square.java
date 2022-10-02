62859d539a3eda1a1509877af2dd8f9f3dcb8369
Trisha Gee <trisha.gee@gmail.com>
2022-10-02 20:46:20 +0200

    Formatting to 2 spaces instead of 4

--- src/main/java/com/getting2knowij/running/Square.java
+++ src/main/java/com/getting2knowij/running/Square.java
@@ -1,23 +1,23 @@
 package com.getting2knowij.running;
 
 public class Square {
-    private final int sides = 4;
-    private final String label;
+  private final int sides = 4;
+  private final String label;
 
-    public Square(String label) {
-        this.label = label;
-    }
+  public Square(String label) {
+    this.label = label;
+  }
 
-    public int getSides() {
-        return sides;
-    }
+  public int getSides() {
+    return sides;
+  }
 
-    @Override
-    public String toString() {
-        String str = "Square{" +
-                "sides=" + sides +
-                ", label='" + sides + '\'' +
-                '}';
-        return str;
-    }
+  @Override
+  public String toString() {
+    String str = "Square{" +
+                 "sides=" + sides +
+                 ", label='" + sides + '\'' +
+                 '}';
+    return str;
+  }
 }
